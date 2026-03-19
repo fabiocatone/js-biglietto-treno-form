94a7007d398bc1e204d53274631f80ea90ecca42
fabiocatone <fabbiocatone@email.com>
2026-03-19 17:01:59 +0100

    finish all not yet design

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-const howKm = document.getElementById("km")
-const howAge = document.getElementById("age")
-const buttonEl = document.getElementById("btn")
+const howKm = document.getElementById("kmuser")
+const howAge = document.getElementById("ageuser")
+const formEl = document.querySelector("form")
+const nameEl = document.getElementById("nameuser")
 
-buttonEl.addEventListener("click", function (event) {
-    event.preventDefault;
+
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault(event);
     let cost = howKm.value * 0.21;
+    let ticketname = document.getElementById("ticket-name");
+    let ticketoffer = document.getElementById("tariffa")
+let ticketcarr = document.getElementById("Carrozza")
+let ticketcod = document.getElementById("Codice")
+let ticketcost = document.getElementById("Costo")
         console.log(howKm.value);
     if (howAge.value < 18) {
         cost = cost * 0.8
+        ticketoffer.innerText="Biglietto Under 18"
     }
     else if (howAge.value > 65) {
         cost = cost * 0.6
+        ticketoffer.innerText = "Biglietto Over 65"
+    }
+    else {
+        ticketoffer.innerText= "Biglietto Standard"
     }
 
     console.log(cost);
+ticketname.innerText = nameEl.value
+ticketcarr.innerText = "7"
+ticketcod.innerText = "47389"
+ticketcost.innerText = cost
+
+
+
+}
+
     
-})
+)
 
